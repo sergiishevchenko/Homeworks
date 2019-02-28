2f582a981f83064c6be25aa53e355c9be3d0039e
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-02-28 22:14:33 +0300

    Add files via upload

--- lesson-14/src/js/main.js
+++ lesson-14/src/js/main.js
@@ -2,12 +2,13 @@ var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function(e) {
     var n = 5000;
     modal.classList.add('modal_active');
     setTimeout(function() {
         document.getElementById('modal').classList.remove('modal_active');
     }, n);
+    e.preventDefault();
 });
 
 close.addEventListener('click', function() {
