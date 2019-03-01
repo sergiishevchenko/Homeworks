38b541742099cdb74b8558caf2eedf0b44005cf3
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-03-01 12:12:12 +0300

    lesson 18 main

--- lesson-14/src/js/main.js
+++ lesson-14/src/js/main.js
@@ -5,9 +5,10 @@ var close = document.querySelector('#close');
 button.addEventListener('click', function(e) {
     var n = 5000;
     modal.classList.add('modal_active');
-    setTimeout(function() {
+    var t = setTimeout(function() {
         document.getElementById('modal').classList.remove('modal_active');
     }, n);
+    console.log(t);
     e.preventDefault();
 });
 
